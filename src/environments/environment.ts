c717c5d793d090a752f0bada0a0f39046fc40d9c
Ryan Pascal <ryan.david.pascal@gmail.com>
2019-06-30 21:37:53 -0400

    removing bad things

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDxap4m94JZ6Stvt6FAhQjahM_G_7o3dIk",
+    apiKey: "",
     authDomain: "lafayetteumc-330a6.firebaseapp.com",
     databaseURL: "https://lafayetteumc-330a6.firebaseio.com",
     projectId: "lafayetteumc-330a6",
